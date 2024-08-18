977112a08207484b8bd87bf72802b46d0d041696
ProgressAce <daiyaansallie7@gmail.com>
2024-08-18 11:08:03 +0200

    add: configure application
    app.py is the entry point of the application and makes use of `create_app`
    to initialise the app. The `create_app` factory function which initialises
    and configures the entire application based on the specified
    environment. The environments are for "development", "testing" and
    "production".
    The environments' configuration differ according to the
    attribute values stored in their respective class. A private .env file
    stores the secret configuration values that should remain unknown. The
    Mongo database connection settings are stored here as well.

---
+++ config.py
@@ -0,0 +1,37 @@
+"""Defines the app's Environment-Specific Configuration for Flask."""
+from os import environ, path
+from dotenv import load_dotenv
+
+# Specify the .env file containing the key/value config values
+base_dir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(base_dir, '.env'))
+
+
+class Config(object):
+    """The base setup for a Flask app's configuration."""
+    SECRET_KEY = environ.get('SECRET_KEY')
+    DEBUG = False
+    TESTING = False
+
+
+class ProductionConfig(Config):
+    """The flask configuration for a production environment."""
+    DEBUG = False  # ensure debug mode is false
+
+
+class DevelopmentConfig(Config):
+    """The flask configuration for a development environment."""
+    DEBUG = environ.get('DEBUG')
+
+
+class TestingConfig(Config):
+    """The flask configuration for a testing environment."""
+    TESTING = environ.get('TESTING')
+    DEBUG = True
+    WTF_CSRF_ENABLED = False  # Disable CSRF for testing
+    PRESERVE_CONTEXT_ON_EXCEPTION = False  # Prevents exceptions from propagating
+    # a separate database for tests
+    # the test database's connection name is stored in the .env file
+    MONGODB_SETTINGS = {
+        'db': environ.get('TEST_DB'),
+    }
