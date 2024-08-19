aa5da230a905192b6021dc90fc2d8a48f6f233dc
ProgressAce <daiyaansallie7@gmail.com>
2024-08-19 09:12:01 +0200

    refactor: move models directory

--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Imports all the database models for easier access."""
-# TODO: sort out circular import error before using
-# from app.models.household import Household
-# from app.models.user import User
