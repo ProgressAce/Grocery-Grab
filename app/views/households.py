a11414d0a10739e4a7ec48bd9108889665fd6f09
ProgressAce <daiyaansallie7@gmail.com>
2024-08-22 14:10:04 +0200

    Add route to create a household
    Retrieve the 'name' and 'password' as json input from the
    request. Validate them. Ensure that the 'name' is unique compared
    to other existing household names. Hash the password and create a new
    household object with the hashed_password, name and record the user
    creating the household as its first admin and normal member.
    Update the user to indicate the household they belong to.
    
    Apart from the using the input to create a new household object and
    Mongo DB document, the logged-in user and the new household form a
    one-to-many relationship where a household can contain many users. The
    household is however, only created when a user requires it and
    therefore a household is dependant on users for it to exist in this
    application. (A household does not exist if it doesn't have any
    members)

--- app/views/households.py
+++ app/views/households.py
@@ -35,3 +35,44 @@ def household_member_required(view_func):
         # If everything is fine, proceed to the route
         return view_func(*args, **kwargs)
     return decorated_function
+
+
+@household_bl.post('/households', strict_slashes=False)
+@login_required
+def create_household():
+    """CREATE a new household."""
+
+    try:
+        # TODO: catch exception when given type of not application/json
+        data: dict = request.get_json()
+
+        if not data:
+            return jsonify({"error": "No user data provided"}), 400
+
+        name = data.get('name')
+        password = data.get('password')
+
+        # Check if the house name is provided
+        if not name:
+            return jsonify({"error": "Name is required"}), 400
+
+        # Ensure house name is unique
+        user_by_username = Household.objects(name=name).first()
+        if user_by_username:
+            return jsonify({'error': 'The name is already taken'}), 400
+
+        if not is_valid_password(password):
+            return jsonify({'error': 'Password must be at least 8 characters long'
+                            + ' and meet complexity requirements'}), 400
+
+        password_hash = generate_password_hash(password)
+
+        household = Household(name=name, password_hash=password_hash,
+                              members=[current_user], admins=[current_user])
+        household.save()
+        current_user.household_id = household.id
+        current_user.save()
+
+        return jsonify({"message": "Household created successfully"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
